fcfa982109d8edace29c2a490db726a651ea422e
Francesc Bagué Martí <francesc.bague@gmail.com>
2023-05-25 01:30:22 +0200

    Diseño acavado

--- script.js
+++ script.js
@@ -1,72 +1,84 @@
-// function setAction() {
-//     // const form = document.forms['myform']
-//     // console.log('form',form)
-//     // location.href = `./result.html?name=${form['name'].value}&age=${form['age'].value}`
-//     return false
-// }
-// document.getElementById('myform').onsubmit = 'event.preventDefault(); return setActio;n'
-// import FormData from 'https://cdn.jsdelivr.net/npm/form-data@4.0.0/+esm'
-document.querySelector('#fileInput').addEventListener('change', (event) => {
+const selected = document.querySelector('#selectedImage')
+const returned = document.querySelector('#returnedImage')
+const footer   = document.querySelector('.card-footer')
+function updateImage(event) {
+    selected.classList.remove('hidden')
+    returned.classList.add('hidden')
+    footer.classList.add('hidden')
     setImage('selected', event.target.files[0])
-})
-let fileselected = File
+}
+// document.querySelector('#fileInput').addEventListener('change', (event) => {
+//     setImage('selected', event.target.files[0])
+// })
 let fd = FormData
-function removebg(event) {
+function removebg(event, intent=0) {
     event.preventDefault();
-    // const key = 'MhGo4KGWj2ZKqJzUvaJzGz6s';
-    const key = 'htmEDHT5syLgYxcUcsbPQQqE'
+    // const key = 'MhGo4KGWj2ZKqJzUvaJzGz6s' // key fbaguem2021
+    // const key = 'htmEDHT5syLgYxcUcsbPQQqE' // key pruevas
+
+    const keys = [ 'htmEDHT5syLgYxcUcsbPQQqE', 'MhGo4KGWj2ZKqJzUvaJzGz6s' ]
+    const key = (function(intent) {
+        if (intent < keys.length) return keys[intent]
+        else return -1
+    })(intent)
+    if (key === -1) return false
+
     const fileInput = document.forms['myform']['file']
-    const headers = { axios: Object, fetch: Object }
-    const formData = new FormData()
-    // console.log(fileInput.files[0]);
-    fileselected = fileInput.files[0]
+    let headers = { axios: Object, fetch: Object }
+    let formData = new FormData()
+    let fileselected = fileInput.files[0]
     formData.append('image_file', fileInput.files[0])
     formData.append('size','auto')
     fd = formData
-    // headers.axios = getAxiosHeaders(formData, key)
     headers.fetch = getFetchHeaders(formData, key)
 
     fetch('https://api.remove.bg/v1.0/removebg', headers.fetch)
     .then(response => response.blob())
     .then(blob => {
+        console.log(1);
         const fileData = {
             extension: ((file) => { const arr=file.split('.');return arr[arr.length-1] })(fileselected.name),
-            curdate: new Date().toLocaleString().replace(', ','-'),
-            name: `no-background-image-${this.curdate}.${this.extension}`,
+            curdate:   new Date().toLocaleString().replace(', ','-'),
+            name:      `no-background-image-${this.curdate}.${this.extension}`,
         }
         const file = new File([blob], fileData.name, { type: blob.type })
         setImage('returned', file)
         
         const downloadbtn = document.querySelector('#downloadbtn')
         downloadbtn.classList.remove('hidden')
-        downloadbtn.addEventListener('click', (event) => {
-            const link = document.createElement('a')
-            link.href = URL.createObjectURL(file)
+        const handler = function(file) {
+            const link    = document.createElement('a')
+            link.href     = URL.createObjectURL(file)
             link.download = file.name
             document.body.appendChild(link)
             link.click()
             document.body.removeChild(link)
-        })
+        } 
+        handler(file)
+        downloadbtn.addEventListener('click', (event) => { handler(file) })
         console.log("The background of the image has been removed successfully");
     })
     .catch(error => {
+        intent++
         console.error('Error: ',error)
+        removebg(event, intent)
     })
 
     return false
 }
 function setImage(type, image) {
     const reader = new FileReader();
-    // let selectedImage = document.querySelector('#selectedImage')
     reader.addEventListener('load', (event) => {
         if (type == 'selected') {
-            selectedImage.src = event.target.result
+            selected.src = event.target.result
         } else if (type == 'returned') {
-            returnedImage.src = event.target.result
+            window.localStorage.removedImage = image
+            returned.src = event.target.result
+            returned.classList.remove('hidden')
+            footer.classList.remove('hidden')
         }
     });
     reader.readAsDataURL(image);
-    
 }
 function getAxiosHeaders(formData, key) {
     return {
@@ -83,9 +95,7 @@ function getAxiosHeaders(formData, key) {
 function getFetchHeaders(formData, key) {
     return {
         method: 'POST',
-        headers: {
-            'X-Api-Key': key
-        },
+        headers: { 'X-Api-Key': key },
         body: formData
     }
 }
@@ -96,4 +106,3 @@ function windowLoad(event) {
     }
 }
 window.addEventListener('load',windowLoad)
-// document.querySelector('#myform').addEventListener('submit',removebg)
